2f2fe080494480a5c11012ce6a165dafb92082de
Selma Elgabalawy <salmosha88@gmail.com>
2021-01-23 21:57:36 -0800

    deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -32,5 +32,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-// service workers
